3a1c7fa281e27a0202b985c3305df3b337e57feb
Nick S <nicks@Workbook1.local>
2014-08-27 11:38:17 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+app.get('/', function(request, response) {
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
+});
